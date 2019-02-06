ddf585758d701c5276495643e99c349242f5ea2d
camgelozzi <cangelozzi@gmail.com>
2019-02-06 14:00:25 -0500

    linked frontend and backend

---
+++ js/components/UsersComponent.js
@@ -0,0 +1,13 @@
+export default {
+  template: `
+    <h1>You are in the User Page</h1>
+  `,
+
+  data() {
+    return {
+      message: 'hello from the composer',
+      userList: []
+    }
+  }
+
+};
